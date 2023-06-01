2574ceaafcaa72d1137a4ab82c86a2ba543f67d7
bibolbd <bilguundalai3355@gmail.com>
2023-06-01 23:10:32 +0800

    fix: render board

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-const GameBoard = () => {
-  let gameboard = ["X", "O", "X", "O", "X", "O", "O", "X", "O"];
-  return gameboard;
-};
+const gameBoard = (() => {
+  let gameboard = Array(9);
 
-const gameboard = GameBoard();
+  const setCell = (index, value) => {
+    gameboard[index] = value;
+  };
 
-const renderGameBoard = (gameboard) => {
-  gameboard.map((cellValue, cellIndex) => {
-    const cellElement = document.querySelector(
-      `#main-container > div:nth-child(${cellIndex + 1})`
-    );
-    cellElement.textContent = cellValue;
-  });
-};
+  const resetBoard = () => {
+    gameboard.forEach((_, index) => {
+      gameboard[index] = undefined;
+    });
+  };
 
-renderGameBoard(gameboard);
+  const renderGameBoard = (gameboard) => {
+    gameboard.map((cellValue, cellIndex) => {
+      const cellElement = document.querySelector(
+        `#main-container > div:nth-child(${cellIndex + 1})`
+      );
+      cellElement.textContent = cellValue;
+    });
+  };
+
+  const addMarker = () => {
+    const cells = document.querySelectorAll('[id^="cell-"]');
+    cells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        const cellId = parseInt(cell.id.replace("cell-", ""));
+        console.log("Clicked cell:", cellId);
+        setCell(cellId - 1, "X");
+        renderGameBoard(gameboard);
+      });
+    });
+  };
+
+  const clearBoard = () => {
+    const resetButton = document.querySelector("#resetButton");
+    resetButton.addEventListener("click", () => {
+      resetBoard();
+      renderGameBoard(gameboard);
+    });
+  };
+
+  return {
+    gameboard,
+    clearBoard,
+    addMarker,
+  };
+})();
+
+const gameboard = gameBoard.gameboard;
+gameBoard.addMarker();
+gameBoard.clearBoard();
