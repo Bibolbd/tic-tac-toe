48ed866281f43a05258dd14bbaa8883db5da5ea9
bibolbd <bilguundalai3355@gmail.com>
2023-06-02 12:21:57 +0800

    feat: choose marker

--- script.js
+++ script.js
@@ -26,15 +26,21 @@ const gameBoard = (() => {
       cell.addEventListener("click", () => {
         const cellId = parseInt(cell.id.replace("cell-", ""));
         console.log("Clicked cell:", cellId);
-        setCell(cellId - 1, "X");
-        renderGameBoard(gameboard);
+        gameboard[cellId - 1] === undefined
+          ? (setCell(cellId - 1, gameController.getCurrentPlayer().marker),
+            renderGameBoard(gameboard),
+            gameController.displayPlayer(
+              gameController.getCurrentPlayer().name
+            ),
+            gameController.switchPlayer())
+          : console.log("Put your mark on emtpy cell!");
       });
     });
   };
 
   const clearBoard = () => {
-    const resetButton = document.querySelector("#resetButton");
-    resetButton.addEventListener("click", () => {
+    const restartButton = document.querySelector("#restartButton");
+    restartButton.addEventListener("click", () => {
       resetBoard();
       renderGameBoard(gameboard);
     });
@@ -47,6 +53,45 @@ const gameBoard = (() => {
   };
 })();
 
+const gameController = (() => {
+  const Player = (name, marker) => {
+    return { name, marker };
+  };
+
+  let player1 = Player("player1", "O");
+  let player2 = Player("player2", "X");
+  let currentPlayer = player1;
+
+  const chooseMarker = () => {
+    const choiceDisplayer = document.querySelector("#chooseMarker");
+    choiceDisplayer.textContent = "choose your marker Player1";
+    const markerButtons = document.querySelectorAll('[id^="marker"]');
+    markerButtons.forEach((markerButton) => {
+      markerButton.addEventListener("click", () => {
+        player1.marker = markerButton.textContent;
+        player2.marker = player1.marker === "O" ? "X" : "O";
+        choiceDisplayer.textContent = "";
+      });
+    });
+  };
+
+  const getCurrentPlayer = () => {
+    return currentPlayer;
+  };
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
+
+  const displayPlayer = (name) => {
+    const displayer = document.querySelector("#currentPlayer");
+    displayer.textContent = `${name}'s turn`;
+  };
+
+  return { getCurrentPlayer, switchPlayer, displayPlayer, chooseMarker };
+})();
+
 const gameboard = gameBoard.gameboard;
+gameController.chooseMarker();
 gameBoard.addMarker();
 gameBoard.clearBoard();
